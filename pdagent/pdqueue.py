90fa502a994bf60f27dbece559ce0952ec2d8816
kamsrikanth <55290046+kamsrikanth@users.noreply.github.com>
2019-10-10 11:44:22 -0500

    DOCD

--- pdagent/pdqueue.py
+++ pdagent/pdqueue.py
@@ -25,7 +25,9 @@
 # CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
-#
+
+
+#Base class for queue & enqueue. Also constructs get status
 """
 A directory based queue for PagerDuty events.
 
