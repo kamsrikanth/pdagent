281431380044cc3ab39d891868a49e0cb111d2e9
kamsrikanth <55290046+kamsrikanth@users.noreply.github.com>
2019-10-10 11:46:05 -0500

    DOCD

--- pdagent/pdthread.py
+++ pdagent/pdthread.py
@@ -25,7 +25,8 @@
 # CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
-#
+
+#Intialize and configure repeating task thread
 
 import logging
 from threading import Thread
